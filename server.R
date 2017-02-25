64f868f3d4de9dd73e5968c34e2c20b186c9c644
Kristen Crasto <kmc376@cornell.edu>
2017-02-25 14:34:33 -0500

    Ethan doing stuff

--- server.R
+++ server.R
@@ -1,9 +1,15 @@
-
 library(twitteR)
 library(RCurl)
 library(shiny)
+library(wordcloud)
+library(tm)
 
 shinyServer(function(input, output) {
   
-  
+  output$wordcloud <- renderPlot({
+    topic_tweets=searchTwitter(c(input$topic),lang="en",n=500,resultType = "recent")
+    him_corpus=corpus(VectorSource(topic_tweets))
+    him_corpus=tm_map(him_corpus,removePunctuation)
+    
+  })
 })

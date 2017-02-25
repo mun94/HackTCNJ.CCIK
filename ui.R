727fe27f5d7ccf5daf7f81daf7fccc7d1d5fb90e
crastoe1 <crastoe1@tcnj.edu>
2017-02-25 12:47:23 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,14 @@
 
+library(twitteR)
+library(RCurl)
+library(shiny)
+
+pageWithSidebar(
+  headerPanel('Creative Name'),
+  sidebarPanel(
+    textInput("topic","Please enter a topic",value="")
+    ),
+  mainPanel(
+    plotOutput("wordcloud")
+  )
+)

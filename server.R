6b11060e30ee3a87d4534eb4de0f9f0c7dcd975a
Kristen Crasto <kmc376@cornell.edu>
2017-02-25 14:40:21 -0500

    why git no work for me

--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(twitteR)
 library(RCurl)
 library(shiny)
@@ -7,27 +6,22 @@ library(tm)
 
 shinyServer(function(input, output) {
   
+  clean_corpus=function(topic){
+    topic_tweets=searchTwitter(topic,lang="en",n=500,resultType = "recent")
+    topic_corpus=Corpus(VectorSource(topic_tweets))
+    topic_corpus=tm_map(topic_corpus,removePunctuation)
+    topic_corpus=tm_map(topic_corpus,content_transformer(tolower))
+    topic_corpus=tm_map(topic_corpus,removewords,stopwords("english"))
+    topic_corpus=tm_map(topic_corpus,removeNumbers)
+    topic_corpus=tm_map(topic_corpus,stripWhitespace)
+    return(topic_corpus)
+  }
+  
   output$wordcloud <- renderPlot({
     topic_tweets=searchTwitter(c(input$topic),lang="en",n=500,resultType = "recent")
     him_corpus=corpus(VectorSource(topic_tweets))
     him_corpus=tm_map(him_corpus,removePunctuation)
-    
+    him_corpus=tm_map(him_corpus,removeNumbers)
+    him_corpus
   })
 })
-=======
-  library(twitteR)
-  library(RCurl)
-  library(shiny)
-  library(wordcloud)
-  library(tm)
-  
-  shinyServer(function(input, output) {
-    
-    output$wordcloud <- renderPlot({
-      topic_tweets=searchTwitter(c(input$topic),lang="en",n=500,resultType = "recent")
-      him_corpus=corpus(VectorSource(topic_tweets))
-      him_corpus=tm_map(him_corpus,removePunctuation)
-      
-    })
-})
->>>>>>> cd448426a9cd22120a581c250552348fbc1c9992

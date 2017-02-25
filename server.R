071fcd7b21ea2ad69379f66e501afb67b8256365
crastoe1 <crastoe1@tcnj.edu>
2017-02-25 15:44:41 -0500

    Defined new server.R

--- server.R
+++ server.R
@@ -6,6 +6,19 @@
   
   shinyServer(function(input, output) {
     
+    clean_corpus=function(topic){
+      topic_tweets=searchTwitter(topic,lang="en",n=500,resultType = "recent")
+      topic_text=sapply(topic_tweets,function(x) x$getText())
+      #tweets_text <- sapply(topic_tweets,function(row) iconv(row, "latin1", "ASCII", sub=""))
+      topic_corpus=Corpus(VectorSource(topic_text))
+      topic_corpus=tm_map(topic_corpus,removePunctuation)
+      topic_corpus=tm_map(topic_corpus,content_transformer(tolower))
+      topic_corpus=tm_map(topic_corpus,removeWords,stopwords("english"))
+      topic_corpus=tm_map(topic_corpus,removeNumbers)
+      topic_corpus=tm_map(topic_corpus,stripWhitespace)
+      return(topic_corpus)
+    }
+    
     output$wordcloud <- renderPlot({
       topic_tweets=searchTwitter(c(input$topic),lang="en",n=500,resultType = "recent")
       him_corpus=corpus(VectorSource(topic_tweets))

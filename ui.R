817fb48eb525231fe3748501c998a99c47e08e3c
Kristen Crasto <kmc376@cornell.edu>
2017-02-26 11:48:40 -0500

    final

--- ui.R
+++ ui.R
@@ -6,11 +6,6 @@ pageWithSidebar(
   headerPanel('Shoot For the Mun'),
   sidebarPanel(
     textInput("topic","Please enter a topic",value="HACKTCNJ"),
-    #dates
-    dateRangeInput("dateRange",
-                   label = "Date range input: yyyy-mm-dd",
-                   start = Sys.Date() - 7, end = Sys.Date()
-    ),
     checkboxInput("useRecent", "Use recent tweets?", value=TRUE),
     actionButton("submit", "Submit")
     ),

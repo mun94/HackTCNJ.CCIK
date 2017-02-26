029f6132beedaf9341a49fc19389398868610d72
crastoe1 <crastoe1@tcnj.edu>
2017-02-26 00:36:06 -0500

    fixing merge conflicts

--- sentiment.R
+++ sentiment.R
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-library(RCurl)
-library(twitteR)
-library(plyr)
-library(stringr)
-library(SentimentAnalysis)
-library(NLP)
-library(slam)
 
-
-consumer_key <- ''
-consumer_secret <- ''
-access_token <- ''
-access_secret <- ''
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-
-
-#setup_twitter_oauth(consumer_key, consumer_secret,access_token, access_secret)
-searchTerm = 'hell'
-tweets <- searchTwitter(searchTerm, n=100, geocode = location, since=min_timeline)
-tweets_text <- sapply(tweets, function(x) x$getText())
-tweets_text_corpus <- Corpus(VectorSource(tweets_text))
-tweets_text_corpus <- tm_map(tweets_text_corpus, content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')), mc.cores=1)
-sentiment <- analyzeSentiment(tweets_text_corpus)
-sentimentWords = convertToDirection(sentiment$SentimentGI)
-=======
 library(RCurl)
 library(twitteR)
 library(plyr)
@@ -85,4 +60,3 @@ tweets_text_corpus <- Corpus(VectorSource(tweets_text))
 tweets_text_corpus <- tm_map(tweets_text_corpus, content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')), mc.cores=1)
 sentiment <- analyzeSentiment(tweets_text_corpus)
 sentimentWords = convertToDirection(sentiment$SentimentGI)
->>>>>>> a5ce6e6b6a184c11372e9fc2473b5c480806ca12
